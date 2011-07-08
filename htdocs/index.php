ecf8cfd0956ed4a4f5ddb1cb06f5e0d3b9c9953f
raetha <github@mindflight.org>
2011-07-07 19:44:03 -0700

    Adding support for running when PHP is in CGI mode.

--- htdocs/index.php
+++ htdocs/index.php
@@ -25,6 +25,10 @@ require_once(dirname(__FILE__) . '/common.inc.php');
 # Authenticate
 
 if (!isset($_SERVER['PHP_AUTH_USER'])) {
+    if (isset($_SERVER['HTTP_AUTHORIZATION'])) {
+        $ha = base64_decode(substr($_SERVER['HTTP_AUTHORIZATION'], 6));
+        list($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']) = explode(':' , $ha);
+    }
     auth_fail();
 }
 
