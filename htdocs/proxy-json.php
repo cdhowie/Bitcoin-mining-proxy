9357b7fd955fbde532629dca3d3780212b1a76a8
edwin <edwin@codis.dominion>
2011-07-13 16:04:57 -0400

    Added ability to run local program to grab stats. Updated config.inc.php.sample with an example. Updated proxy-json to support new 'exec' type for running a local program.

--- htdocs/proxy-json.php
+++ htdocs/proxy-json.php
@@ -10,15 +10,21 @@ global $BALANCE_JSON;
 // Website url to open
 $daurl = $BALANCE_JSON[$_REQUEST['pool']]['url'];
 
-// Get that website's content
-$handle = fopen($daurl, "r");
+if ($daurl) {
+  // Get that website's content
+  $handle = fopen($daurl, "r");
 
-// If there is something, read and return
-if ($handle) {
-    while (!feof($handle)) {
+  // If there is something, read and return
+  if ($handle) {
+      while (!feof($handle)) {
         $buffer = fgets($handle, 64);
-        echo $buffer;
-    }
-    fclose($handle);
+          echo $buffer;
+      }
+      fclose($handle);
+  }
+} else {
+  $execstat = $BALANCE_JSON[$_REQUEST['pool']]['exec'];
+  echo exec($execstat);
 }
+
 ?>
